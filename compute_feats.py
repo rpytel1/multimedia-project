2001b8d9443781b70a6f9c7cc3d1d5bf9e604274
rpytel1 <rafal.pytel1@wp.pl>
2019-06-18 21:38:33 +0200

    Added script for computing image features

---
+++ compute_feats.py
@@ -0,0 +1,36 @@
+import json
+import pickle
+
+from feature_extraction.image.extract_img_feat import extract_feats_from_photo_metadata, extract_cv_feats
+from feature_extraction.image.load_image_util import extract_photo_id, get_photo_info, get_photo_sizes, get_img
+
+
+def get_img_feats(url):
+    photo_id = extract_photo_id(url)
+    photo_info = get_photo_info(photo_id)
+    sizes_info = get_photo_sizes(photo_id)
+    im = get_img(sizes_info)
+    photo_metadata = extract_feats_from_photo_metadata(photo_info)
+    photo_cv_feats = extract_cv_feats(im)
+    photo_metadata.update(photo_cv_feats)
+    return photo_metadata
+
+
+if __name__ == '__main__':
+    with open('data/our_jsons/user_dataset_updated.json') as json_file:
+        user_data = json.load(json_file)
+
+    final_dict = {}
+    i = 0
+    for key, value in user_data.items():
+        user_value = {}
+        for post_key, post_value in value.items():
+            post_value["img_feats"] = get_img_feats(post_value["image_url"])
+            user_value[post_key] = post_value
+            print(i)
+            i += 1
+        final_dict[key] = user_value
+
+    with open('data/our_jsons/user_dataset_computed.pickle', 'wb') as handle:
+        pickle.dump(final_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
--- feature_extraction/image/sift.py
+++ feature_extraction/image/sift.py
@@ -5,7 +5,4 @@ def extract_sift_feats(img):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     sift = cv2.xfeatures2d.SIFT_create()
     kp, dsc= sift.detectAndCompute(gray, None)
-    print(dsc[1,:])
-    print()
-    print(dsc[0, :])
     return dsc
