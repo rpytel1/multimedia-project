8f47c0014f9a2fa7c42634c54803b4b4150ac42b
rpytel1 <rafal.pytel1@wp.pl>
2019-06-19 18:47:47 +0200

    Recommendation system works for multiple users

--- recommendation_system/content_based/reccomendation_system.py
+++ recommendation_system/content_based/reccomendation_system.py
@@ -10,25 +10,22 @@ post_cos_matrix = {}
 train_set = {}
 test_set = {}
 cosine_matrix = {}
+metrics={}
+
+def get__usr_to_post_dict(user_data):
+    usr_to_post_dict_train[key] = list(user_data["train_set"].keys())
 
 
-def get__usr_to_post_dict(data):
-    for key, value in data.items():
-        usr_to_post_dict_train[key] = list(value["train_set"].keys())
+def get_post_to_usr_dict(user_data):
+    for post_key, post_value in user_data["test_set"].items():
+        post_to_usr_test[post_key] = key
 
 
-def get_post_to_usr_dict(data):
-    for key, value in data.items():
-        for post_key, post_value in value["test_set"].items():
-            post_to_usr_test[post_key] = key
-
-
-def prepare_for_matrix_calculations(data, feature_type):
-    for key, value in data.items():
-        for post_key, post_value in value["train_set"].items():
-            train_set[post_key] = post_value[feature_type]
-        for post_key, post_value in value["test_set"].items():
-            test_set[post_key] = post_value[feature_type]
+def prepare_for_matrix_calculations(user_data, feature_type):
+    for post_key, post_value in user_data["train_set"].items():
+        train_set[post_key] = post_value[feature_type]
+    for post_key, post_value in user_data["test_set"].items():
+        test_set[post_key] = post_value[feature_type]
 
 
 def create_empty_cosine_sim_matrix():
@@ -49,17 +46,30 @@ def calculate_cosine(elem1, elem2):
 
 def get_recommendations(top_k):
     df = pd.DataFrame.from_dict(cosine_matrix)
+    print(df)
+    print()
     df = df.sum(axis=1)
+    print(df)
     df = df.nlargest(top_k)
     return df.index.tolist()
 
 
+def clear_all():
+    cosine_matrix.clear()
+    train_set.clear()
+    test_set.clear()
+
+# def calculate_metrics():
+
 if __name__ == '__main__':
     with open('../../data/our_jsons/test.json') as json_file:
         data_json = json.load(json_file)
-    get__usr_to_post_dict(data_json)
-    get_post_to_usr_dict(data_json)
-    prepare_for_matrix_calculations(data_json, "all")
-    create_empty_cosine_sim_matrix()
-    calculate_cosine_sim_matrix()
-    get_recommendations(1)
+    for key, value in data_json.items():
+        get__usr_to_post_dict(value)
+        get_post_to_usr_dict(value)
+        prepare_for_matrix_calculations(value, "all")
+        create_empty_cosine_sim_matrix()
+        calculate_cosine_sim_matrix()
+        recommendations = get_recommendations(1)
+        # calculate_metrics(recommendations)
+        clear_all()
