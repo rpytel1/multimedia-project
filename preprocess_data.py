56b77bb536afd39ea05de335cb2b130e26948811
LENOVO-PC\Vasilis <el12021@central.ntua.gr>
2019-06-22 17:21:42 +0200

    Image features added to the dataset

--- preprocess_data.py
+++ preprocess_data.py
@@ -1,8 +1,15 @@
 import numpy as np
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
-import json
 import pickle
+
+
+def apply_one_hot(df, categorical_features):
+    for col in categorical_features:
+        dummies = pd.get_dummies(df[col], prefix=col)
+        df = pd.concat([df, dummies], axis=1)
+        df.drop(col, axis=1, inplace=True)
+    return df
 
 
 def make_category(data):
@@ -36,6 +43,13 @@ def make_dates(data):
     return df
 
 
+def make_image_fts(data):
+    data_lst = [[pid] + data[pid]['hsv_hist'] + [i for sub in data[pid]['hog'] for i in sub] for pid in data.keys()]
+    headers = ['Pid'] + ['hsv_hist_' + str(i) for i in range(24)] + ['hog_' + str(i) for i in range(144)]
+    df = pd.DataFrame(data_lst, columns=headers)
+    return df
+
+
 def split_data(data, usr_data):
     final_dict = {}
     for user in usr_data.keys():
@@ -47,19 +61,16 @@ def split_data(data, usr_data):
 
 
 if __name__ == '__main__':
-    with open('data/our_jsons/user_dataset_updated.json') as json_file:
-        user_data = json.load(json_file)
-
-    # with open("data/our_jsons/user_dataset_computed.pickle", "rb") as input_file:
-    #     complete_data = pickle.load(input_file)
+    with open("data/our_jsons/user_dataset_computed.pickle", "rb") as input_file:
+        complete_data = pickle.load(input_file)
 
     # split data to feature categories
     category_dict = {}
     tags_dict = {}
-    # image_dict = {} # TODO: implement it when image features are available
+    image_dict = {}
     dates_dict = {}
-    for user in user_data.keys():
-        for pid, vals in user_data[user].items():
+    for user in complete_data.keys():
+        for pid, vals in complete_data[user].items():
             for feat_type in vals.keys():
                 if feat_type in ['Category', 'Concept', 'Subcategory']:
                     if pid not in category_dict.keys():
@@ -71,8 +82,9 @@ if __name__ == '__main__':
                         tags_dict[pid] = {feat_type: vals[feat_type]}
                     else:
                         tags_dict[pid][feat_type] = vals[feat_type]
-                # elif feat_type == 'img_feats':
-                #     image_dict[pid][feat_type] = vals[feat_type]
+                elif feat_type == 'img_feats':
+                    image_dict[pid] = {'hsv_hist': vals[feat_type]['hsv_hist'].tolist(),
+                                       'hog': vals[feat_type]['hog'].tolist()}
                 elif feat_type == 'Postdate':
                     if pid not in dates_dict.keys():
                         dates_dict[pid] = {feat_type: vals[feat_type]}
@@ -82,11 +94,14 @@ if __name__ == '__main__':
     category_data = make_category(category_dict)
     # tags_data = make_tags(tags_dict)  #TODO: fix problem with dimensions
     dates_data = make_dates(dates_dict)
+    image_data = make_image_fts(image_dict)
 
-    all_data = pd.merge(dates_data, category_data, on="Pid")
+    all_data = pd.merge(pd.merge(dates_data, category_data, on="Pid"), image_data, on="Pid")
     # all_data = pd.concat([pd.merge(dates_data, category_data, on="Pid"), tags_data.drop([0], axis=1)], axis=1)
     all_data = all_data.set_index('Pid')
 
-    final_data = split_data(all_data, user_data)
+    final_data = split_data(all_data, complete_data)
+    with open('data/our_jsons/final_dataset.pickle', 'wb') as handle:
+        pickle.dump(final_data, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
     print('preprocessing completed!!!')
